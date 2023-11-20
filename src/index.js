5ac30d4bd8eb13257c4da9ac1d3d969d77476684
Santiago Acevedo Ruiz <151090511+GoldamVictor@users.noreply.github.com>
2023-11-20 18:56:55 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is now working");
+const usuarios = [
+  { email: "xx@gmail.com", password: "123" },
+  { email: "zzz@hotmail.com", password: "user2" },
+  { email: "asdf@outlook.com", password: "user3" },
+];
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", ({}, res) => {
+  return res.send("Server is now working");
+});
+
+app.post("/auth", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const usuarioAutenticado = usuarios.find(
+    (u) => u.email === email && u.password === password,
+  );
+
+  if (usuarioAutenticado) {
+    return res
+      .status(300)
+      .send({ email: usuarioAutenticado.email, isValid: true });
+  }
+  return res.status(401).send("Credenciales Invalidas");
 });
 
 app.listen(3000, () => {
